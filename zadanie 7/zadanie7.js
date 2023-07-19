e86c69fbc0cb5a34e7decc928b43f57029ef7af4
konrad <kondelekk@gmail.com>
2023-07-19 23:04:52 +0200

    zadanie

---
+++ zadanie 7/zadanie7.js
@@ -0,0 +1,10 @@
+const firstNumber = 2;
+const secondNumber = 5;
+
+function addition(num1, num2) {
+  const result = num1 + num2;
+  console.log(result);
+  return;
+}
+
+addition(firstNumber, secondNumber);

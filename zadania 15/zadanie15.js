142647f9a731284e67d37974694b7e0b7b84caa1
konrad <kondelekk@gmail.com>
2023-07-21 09:34:12 +0200

    zadanie

---
+++ zadania 15/zadanie15.js
@@ -0,0 +1,11 @@
+function num(num1, num2) {
+  const add = num1 + num2;
+  const sub = num1 - num2;
+  const multi = num1 * num2;
+
+  console.log(`wynik z dodawania wynosi ${add}`);
+  console.log(`wynik z odejmowania wynosi ${sub}`);
+  console.log(`wynik z mnożenia wynosi ${multi}`);
+}
+
+num(22, 11);

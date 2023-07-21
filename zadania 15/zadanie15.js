3f4cfb2df9c33e934c4511c11aeb5a2904d2c605
konrad <kondelekk@gmail.com>
2023-07-21 22:12:10 +0200

    zadanie

--- zadania 15/zadanie15.js
+++ zadania 15/zadanie15.js
@@ -3,13 +3,13 @@ function num(num1, num2) {
   const sub = num1 - num2;
   const multi = num1 * num2;
 
-  if (add < 0 || sub < 0 || multi < 0) {
-    console.log(`Wynik jest nieprawidłowy`);
-  }
-
   console.log(`wynik z dodawania wynosi ${add}`);
   console.log(`wynik z odejmowania wynosi ${sub}`);
   console.log(`wynik z mnożenia wynosi ${multi}`);
+
+  if (add < 0 || sub < 0 || multi < 0) {
+    console.log(`Wynik jest nieprawidłowy`);
+  }
 }
 
 num(22, 11);

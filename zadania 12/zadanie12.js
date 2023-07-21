1db8f6b9e440d549e213131bd5e1d3eee3527403
konrad <kondelekk@gmail.com>
2023-07-21 09:18:22 +0200

    zadanie

--- zadania 12/zadanie12.js
+++ zadania 12/zadanie12.js
@@ -2,7 +2,11 @@ const loveJs = "Uwielbiam JavaScript";
 const great = "Jestem świetnym programistą";
 
 function add(par1, par2) {
-  console.log(`${par1} ${par2}`);
+  if (par1.length > par2.length) {
+    console.log(`${par1}`);
+  } else {
+    console.log(`${par2}`);
+  }
 }
 
 add(loveJs, great);

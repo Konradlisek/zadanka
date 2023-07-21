894ebb9b32dbf4c149289768bffb6f80941ff3db
konrad <kondelekk@gmail.com>
2023-07-21 09:11:49 +0200

    zadanie

--- zadanie 10/zadanie10.js
+++ zadanie 10/zadanie10.js
@@ -2,8 +2,11 @@ const num1 = 2;
 const num2 = 5;
 
 function add(a, b) {
-  const multiplication = a * b;
-  console.log(multiplication);
+  if (a > b) {
+    console.log(`${a} `);
+  } else {
+    console.log(`${b}`);
+  }
 }
 
 add(num1, num2);
